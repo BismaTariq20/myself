5fb9bd934dec7df603db3615be559f674e6e2a0a
BismaTariq20 <bismatariq873@gmail.com>
2024-08-02 08:48:08 -0700

    commit6

--- myself.py
+++ myself.py
@@ -3,4 +3,5 @@ I am from Faisalabad.
 My father name is Tariq Mehmood.
 My strength are I am self motivating person.
 I have strong communication skills.
-Ihave completed my Intermediate FSc Medical.
+I have completed my Intermediate FSc Medical.
+I am stong and multi tasker.
